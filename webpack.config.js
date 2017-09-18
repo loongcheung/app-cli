684f45f31f82fb134c97d3bacfb085fd4dab5061
jackiedark <jackiedark@qq.com>
2017-09-18 13:49:08 +0800

    20170918

---
+++ webpack.config.js
@@ -0,0 +1,83 @@
+/**
+ * Created by zhanglongyu on 2017/9/15.
+ */
+let path = require('path');
+let webpack = require('webpack');
+let HtmlWebpackPlugin = require('html-webpack-plugin');
+let ExtractTextPlugin = require('extract-text-webpack-plugin');
+let CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: {
+        index: path.resolve(__dirname, 'src', 'scripts', 'index.js'),
+        //second: path.resolve(__dirname, 'src', 'scripts', 'second.js'),  //多个页面添加入口
+    },
+    output: {
+        filename: 'scripts/[name]-[hash].js',
+        chunkFilename: "scripts/[name].chunk.js",
+        path: path.resolve(__dirname, 'dist'),
+        /*publicPath: "/dist/"*/
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                include: path.resolve(__dirname, 'src', 'scripts')
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader','sass-loader']
+                }),
+                include: path.resolve(__dirname, 'src', 'styles')
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                loader: 'url-loader',
+                query: {
+                    limit: 10000,
+                    name: 'img/[name].[hash:8].[ext]'
+                }
+            }
+        ]
+    },
+    plugins: [
+        /* new webpack.optimize.UglifyJsPlugin({ //代码压缩
+         compress: {
+         warnings: false
+         }
+         }),*/
+        new ExtractTextPlugin({
+            filename: './styles/[name].css',
+        }),  //单独打包css
+        new CopyWebpackPlugin([{    //合并static文件下文件
+            from: path.resolve(__dirname, 'static'),
+            to: path.resolve(__dirname, 'dist', 'static'),
+            ignore: ['.*']
+        }]),
+        new HtmlWebpackPlugin({
+            filename: './views/index.html',
+            template: path.resolve(__dirname, 'src', 'views', 'index.html'),
+            inject: 'body',
+            hash: true,
+            chunks: ['index'],
+            minify: {
+                removeComments: true,
+                collapseWhitespace: false
+            }
+        }),
+        /*new HtmlWebpackPlugin({  //多个页面添加入口
+            filename: './views/second.html',
+            template: path.resolve(__dirname, 'src', 'views', 'second.html'),
+            inject: 'body',
+            hash: true,
+            chunks: ['second'],
+            minify: {
+                removeComments: true,
+                collapseWhitespace: false
+            }
+        })*/
+    ]
+};
